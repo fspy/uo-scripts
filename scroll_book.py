9a2d5e59934058befafb005f019e7c4fd4c26655
Felipe Spychalski <45343878+fspy@users.noreply.github.com>
2023-04-27 10:12:06 -0300

    add csv exports to scroll book

--- scroll_book.py
+++ scroll_book.py
@@ -1,9 +1,11 @@
+from collections import namedtuple
 import os
 from AutoComplete import *
 from lib.util import Hue
 
+csv = False
 main_gump = 998728455
-scrolls = {}
+scrolls = []
 
 
 def send_action(btn):
@@ -26,10 +28,13 @@ def navigate(btn):
         send_action(n)
         skill = Gumps.LastGumpGetLineList()[1:]
         Misc.SendMessage('Got skill {}...'.format(skill[0]), Hue.White)
-        scrolls[skill[0]] = [int(n) for n in skill[1:]]
+        scrolls.append(Scroll._make(skill))
         send_action(1)
     send_action(2)
 
+
+Scroll = namedtuple('Scroll',
+                    ['Skill', 'Wondrous', 'Exalted', 'Mythical', 'Legendary'])
 
 Misc.SendMessage('Waiting for you to open the book... (30s)', Hue.Cyan)
 Gumps.WaitForGump(main_gump, 30000)
@@ -41,11 +46,24 @@ for section in parse_buttons(11):
 Misc.SendMessage('Done looking at the book!', Hue.Green)
 Gumps.CloseGump(main_gump)
 
-with open('scroll_data.txt', 'w') as f:
-    tpl = '{:<20} 120: {:<5} 115: {:<5} 110: {:<5} 105: {:<5}\n'
-    for skill, (five, ten, fifteen, twenty) in sorted(scrolls.items()):
-        f.write(tpl.format(skill, twenty, fifteen, ten, five))
+file_name = f"scroll_data.{'csv' if csv else 'txt'}"
+with open(file_name, 'w') as f:
+    if csv:
+        f.write('skill,120,115,110,105\n')
+        for scroll in sorted(scrolls):
+            f.write(f'{scroll.Skill},'
+                    f'{scroll.Legendary},'
+                    f'{scroll.Mythical},'
+                    f'{scroll.Exalted},'
+                    f'{scroll.Wondrous}\n')
+
+    else:
+        for scroll in sorted(scrolls):
+            f.write(f'{scroll.Skill:<20}'
+                    f'120: {scroll.Legendary:<5}'
+                    f'115: {scroll.Mythical:<5}'
+                    f'110: {scroll.Exalted:<5}'
+                    f'105: {scroll.Wondrous}\n')
 
     f.close()
-    Misc.SendMessage(
-        'Wrote to file: "{}\\scroll_data.txt"'.format(os.getcwd()), Hue.Green)
+    Misc.SendMessage(rf'Wrote to file: "{os.getcwd()}\{file_name}"', Hue.Green)
