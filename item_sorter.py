01c1200316773bdf872e4f486e012b759a45b30c
Felipe Spychalski <45343878+fspy@users.noreply.github.com>
2023-05-02 09:40:13 -0300

    try to find good items

--- item_sorter.py
+++ item_sorter.py
@@ -11,33 +11,45 @@ from AutoComplete import *
 from lib.item_query import ItemQuery
 from lib.util import Hue, head_prompt
 
-CONTAINER_WEAPON = 0x40722114
-CONTAINER_UNRAVEL = 0x40722B66
-CONTAINER_GARGOYLE = 0x402E19F0
-CONTAINER_WEIGHTS = {
-    0.35: 0x40316502,
-    0.30: 0x4072208F,
-    0.25: 0x40722B9C,
-    0.20: 0x40722B88,
-    0.15: 0x40722C94
-}
-weights = {
-    # mod name (case insensitive): [weight, maximum value]
-    'lower reagent cost': [4, 25],
-    'lower mana cost': [6, 10],
-    'mana regeneration': [5, 4],
-    'mana increase': [3, 10],
-    'intelligence bonus': [3, 10],
-    'resist': [4, 35],
-    'faster casting': [8, 4],
-    'faster cast recovery': [10, 6]
-}
 
-melee_armor = re.compile(
+def find_gear(source, destination=Player.Backpack.Serial, settings={}):
+    q = ItemQuery(source)
+    if settings.get('resist'):
+        Misc.SendMessage('Moving high resist items...')
+        items = q.amount(ItemQuery.Resist, settings['resist'])
+        items.move_all(destination)
+
+    if settings.get('skill'):
+        Misc.SendMessage('Moving high skill items...')
+        items = q.amount(ItemQuery.Skill, settings['skill'])
+        items.move_all(destination)
+
+    if settings.get('caster'):
+        Misc.SendMessage('Moving caster armor...')
+        items = q.count(caster_armor, settings['caster'])
+        items.move_all(destination)
+
+    if settings.get('casterjewelry'):
+        Misc.SendMessage('Moving caster jewelry...')
+        items = q.count(caster_jewelry, settings['casterjewelry'])
+        items.move_all(destination)
+
+    if settings.get('fighter'):
+        Misc.SendMessage('Moving fighter armor...')
+        items = q.count(fighter_armor, settings['fighter'])
+        items.move_all(destination)
+
+    if settings.get('fighterjewelry'):
+        Misc.SendMessage('Moving fighter jewelry...')
+        items = q.count(fighter_jewelry, settings['fighterjewelry'])
+        items.move_all(destination)
+
+
+fighter_armor = re.compile(
     r'^((strength|dexterity) bonus|(hit point|stamina|mana) increase|'
     r'lower mana cost)', re.IGNORECASE)
 
-melee_jewelry = re.compile(
+fighter_jewelry = re.compile(
     r'^(swing speed increase|((hit|defense) chance|damage) increase|'
     r'(strength|dexterity) bonus)', re.IGNORECASE)
 
@@ -50,29 +62,34 @@ caster_jewelry = re.compile(
     r'lower (mana|reagent) cost)|spell damage increase|'
     r'faster cast(ing| recovery)', re.IGNORECASE)
 
+containers = {
+    'head': 0x40722C94,
+    'neck': 0x40722B88,
+    'chest': 0x40722B9C,
+    'arms': 0x4072208F,
+    'hands': 0x40316502,
+    'legs': 0x40722114,
+    'bracelet': 0x407220A9,
+    'ring': 0x407220C7,
+}
 
-def find_gear(armor, jewelry, resists=0):
-    q = ItemQuery(head_prompt('Where do I look?'))
-    armor = q.count(*armor).amount(ItemQuery.Resist, resists)
-    if armor.items: armor.move_all(Player.Backpack.Serial)
-    jewelry = q.count(*jewelry)
-    if jewelry.items: jewelry.move_all(Player.Backpack.Serial)
+dump_chest = 0x4031654D
 
+settings = {
+    'resist': 70,
+    'skill': 40,
+    'caster': 4,
+    'fighter': 5,
+    'casterjewelry': 5,
+    'fighterjewelry': 4
+}
 
-# find_gear((caster_armor, 4), (caster_jewelry, 5), 55)
-find_gear((melee_armor, 4), (melee_jewelry, 4), 55)
+# for cont in containers.values():
+#     find_gear(cont, Player.Backpack.Serial, settings)
 
-# q = ItemQuery(source=head_prompt('Target source container'))
-# q.move_all(q.query_prop(q.Gargoyle), CONTAINER_GARGOYLE)
-# q.move_all(q.query_prop(q.Weapon), CONTAINER_WEAPON)
-# q.move_all(q.query_prop(q.Unravel), CONTAINER_UNRAVEL)
-
-# # convert to (weight, serial) tuples, ordered by highest weight
-# weight_container = sorted(CONTAINER_WEIGHTS.items(),
-#                           reverse=True,
-#                           key=lambda x: x[0])
-
-# for (weight, container) in weight_container:
-#     q.move_all(q.query_weighted(float(weight), weights), container)
+src = head_prompt('Target container to sort')
+dst = head_prompt('Target where to put items')
+find_gear(src, dst, settings)
+# find_gear(dump_chest, Player.Backpack.Serial, settings)
 
 Misc.SendMessage("Done sorting items!", Hue.Cyan)
