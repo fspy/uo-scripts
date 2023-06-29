0ab6c5bf5b32875ffe71047a53365b864ea8a783
Felipe Spychalski <45343878+fspy@users.noreply.github.com>
2023-06-29 19:43:46 -0300

    arcane focus: - cache skill value on start instead of polling every iteration - only run once a second - look into container memory instead of polling for data  - might break if crystal expires naturally?

--- arcane_focus.py
+++ arcane_focus.py
@@ -1,14 +1,20 @@
 DELAY_IN_SECONDS = 60
 
-Timer.Create('arcane_focus', 1)
-while Player.GetSkillValue('spell weaving') > 0:
-    if Timer.Check('arcane_focus'): # timer hasn't run out, don't do anything
-        Misc.NoOperation()
-        continue
-    if not Items.FindByName('Arcane Focus', 0, Player.Backpack.Serial, True):
-        for i in range(3): 
-            Misc.Beep()
-            Misc.SendMessage('No Arcane Focus!!', 33)
-            Player.HeadMessage(33, 'No Arcane Focus!!')
-            Misc.Pause(250 * (i + 1))
-    Timer.Create('arcane_focus', DELAY_IN_SECONDS * 1000)
+from AutoComplete import *
+
+Timer.Create("arcane_focus", 1)
+skill = Player.GetSkillValue("spell weaving")
+
+while skill > 0:
+    if not Timer.Check("arcane_focus"):  # timer has expired
+        if not next(
+            (i for i in Player.Backpack.Contains if i.Name == "Arcane Focus"), None
+        ):
+            for i in range(3):
+                Misc.Beep()
+                Misc.SendMessage("No Arcane Focus!!", 33)
+                Player.HeadMessage(33, "No Arcane Focus!!")
+                Misc.Pause(250 * (i + 1))
+
+        Timer.Create("arcane_focus", DELAY_IN_SECONDS * 1000)
+    Misc.Pause(1000)
