e7ccbf274168e6c3e2949542428a19d63e077b7f
Felipe Spychalski <felipe@fspy.net>
2026-01-18 21:37:58 -0300

    consistency

--- heartwood_tinker.py
+++ heartwood_tinker.py
@@ -2,7 +2,7 @@ import random
 import re
 
 import API
-from _lib.items import drop_items_to_container, move_item_robust
+from _lib.items import move_item_robust
 from _lib.utils import count_items, p
 
 DEFAULT_PAUSE = 0.65
@@ -32,16 +32,16 @@ def get_quest():
 
         API.UseObject(npc_serial)
         API.WaitForGump(quest_gump)
-
         if not API.HasGump(quest_gump):
             return None
 
         gump_contents = API.GetGumpContents(quest_gump)
         while not gump_contents:
-            API.Pause(0.05)
+            API.Pause(0.3)
 
         match = QUEST_NAME_REGEX.match(gump_contents)
         if not match:
+            attempts -= 1
             continue
 
         if match.group(1) in quests.keys():
@@ -50,9 +50,9 @@ def get_quest():
             return quest_name
 
         API.CloseGump(quest_gump)
-        API.Pause(0.3)
+        API.Pause(DEFAULT_PAUSE * 2)
 
-    p("unable to get quest, exceeded number of attempts")
+    p("unable to get quest, exceeded number of attempts", 36)
     API.Stop()
 
 
@@ -82,12 +82,17 @@ def make_items(category, item, amount, item_id):
 def toggle_quest(id, amount):
     API.ContextMenu(API.Player, 7)
     items = API.FindTypeAll(id, API.Backpack, hue=0)
-    for item in items[:amount]:
-        API.WaitForTarget(timeout=0.5)
-        API.Target(item)  # pyright:ignore
-        API.Pause(0.3)
+
+    count = 0
+    while count < amount:
+        API.WaitForTarget(timeout=3)
+        API.Target(items[count])  # pyright:ignore
+        while not API.InJournal("You set the item to Quest Item status", True):
+            API.Pause(0.05)
+        count += 1
+
     API.CancelTarget()
-    API.Pause(DEFAULT_PAUSE)
+    API.Pause(0.6)
 
 
 def turn_in():
@@ -129,15 +134,17 @@ def find_recipes():
     if not beetle_serial:
         return
 
-    drop_items_to_container(API.FindMobile(beetle_serial).Backpack, [0x2831])
-
-    backpacks = API.FindTypeAll(0x0E75, API.Backpack)
-    for b in backpacks:
-        if b.Serial == API.Backpack:
+    for backpack in API.FindTypeAll(0x0E75, API.Backpack):
+        if backpack.Serial == API.Backpack:
             continue
 
+        API.UseObject(backpack)
+        API.Pause(DEFAULT_PAUSE)
+        for recipe in API.FindTypeAll(0x2831, backpack):
+            move_item_robust(recipe, beetle_serial, 1)
+
         pos = drop_pos()
-        API.MoveItem(b, 4294967295, 1, pos[0], pos[1])
+        API.MoveItem(backpack, 4294967295, 1, pos[0], pos[1])
         API.Pause(DEFAULT_PAUSE)
 
 
@@ -151,17 +158,15 @@ while (
         API.Pause(DEFAULT_PAUSE)
 
     quest_name = get_quest()
-
     if not quest_name:
         API.Pause(DEFAULT_PAUSE)
         continue
 
     category, item, amount, item_id = quests[quest_name]
-    make_items(category, item, amount, item_id)
-    p("toggling quest items")
+    # make_items(category, item, amount, item_id)
+    make_items(*quests[quest_name])
     toggle_quest(item_id, amount)
-    p("turning in")
     turn_in()
 
     API.Pause(DEFAULT_PAUSE)
-    # find_recipes()
+    find_recipes()
